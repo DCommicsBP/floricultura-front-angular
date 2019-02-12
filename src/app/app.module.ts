bf0d3ca9291f44fd9f42b1885dab428870272b64
Daione <daione.pavan@operacao.rcadigital.com.br>
2019-02-12 16:35:05 -0200

    roteamento principal finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PlantasComponent } from './plantas/plantas.component';
@@ -8,6 +8,15 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'plantas', component: PlantasComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'clientes', component:ClientesComponent },
+];
 
 
 @NgModule({
@@ -17,9 +26,11 @@ import { FooterComponent } from './footer/footer.component';
     UsuariosComponent,
     ClientesComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent, 
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
