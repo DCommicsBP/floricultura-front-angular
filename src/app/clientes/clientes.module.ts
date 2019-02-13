a818d438f6b26df049c18d92786b86f53cdcf4ba
Daione <daione.pavan@operacao.rcadigital.com.br>
2019-02-13 10:27:27 -0200

    mais um commit

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientesComponent } from './clientes.component';
 import { ClientesService } from './clientes.service';
-import { ClienteDetalheComponent } from './clientes/cliente-detalhe/cliente-detalhe.component';
+import { ClienteDetalheComponent } from './cliente-detalhe/cliente-detalhe.component';
 
 @NgModule({
   imports: [
